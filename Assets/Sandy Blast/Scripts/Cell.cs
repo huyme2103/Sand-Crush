e69e5383c2dd54a3d732b3ae31c16f3ac7f2da47
huyme2103 <lhuymel2@gmail.com>
2025-10-03 16:49:34 +0700

    simulation sand

---
+++ Assets/Sandy Blast/Scripts/Cell.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public enum EMaterialType
+{
+    Empty = 0,
+    Sand = 1,
+    Water = 2
+}
+public struct Cell 
+{
+    public EMaterialType type;
+    public Color color;
+
+    public static Cell Empty
+        => new Cell 
+        { 
+            type = EMaterialType.Empty,
+            color = Color.clear
+        };
+}
--- Assets/Sandy Blast/Scripts/SandSimulation.cs
+++ Assets/Sandy Blast/Scripts/SandSimulation.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SandSimulation : MonoBehaviour
 {
@@ -8,10 +8,12 @@ public class SandSimulation : MonoBehaviour
 
 
     [Header("Settings")]
-    [SerializeField] private int width;
-    [SerializeField] private int height;
+    [SerializeField] private int width; // x chạy ngang
+    [SerializeField] private int height; // y chạy dọc
     [SerializeField] private Color backgroundColor;
 
+    [Header("Data")]
+    private Cell[,] grid;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,13 +21,136 @@ public class SandSimulation : MonoBehaviour
         texture = new Texture2D(width, height);
         texture.filterMode = FilterMode.Point;
 
+        grid = new Cell[width, height];
+
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                grid[x, y].color = backgroundColor; // duyệt tất cả ô grid thành màu background
+
+        grid[width / 2, height / 2] = new Cell { type = EMaterialType.Sand, color = Color.red };
+
+        UpdateTexture();
+
         renderer = gameObject.AddComponent<SpriteRenderer>();
-        renderer.sprite = Sprite.Create(texture, new Rect(0, 0, width, height), Vector2.one / 2, 100);
+        renderer.sprite = Sprite.Create(texture, new Rect(0, 0, width, height), Vector2.one / 2, 100); //new Rect lấy hết ảnh từ (0,0) đến (width,height)
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Sand Fountain
+        //grid[width / 2, height / 2] = new Cell { type = EMaterialType.Sand, color = Color.red };
+        HandleInput();
+
+        SimulateSand();
+        UpdateTexture();
+    }
+    private void HandleInput()
+    {
+        if (!Input.GetMouseButton(0))
+            return;
+        // click position, texture space
+        Vector3 worldClickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2Int gridCoords = WorldToGrid(worldClickedPosition);
+        if (!IsInBounds(gridCoords))
+            return;
+        //set grid[position] color
+        Color color = Random.ColorHSV(0, 1, .8f, 1f, .8f, 1);
+        grid[gridCoords.x, gridCoords.y] = new Cell { type = EMaterialType.Sand, color = color };
+    }
+
+    private bool IsInBounds(Vector2Int coords)
+    {
+        return coords.x >= 0 && coords.x < width && coords.y >= 0 && coords.y < height;
+    }
+    private Vector2Int WorldToGrid(Vector3 worldPos)
+    {
+        int x = Mathf.RoundToInt(worldPos.x * 100 + width / 2);
+        int y = Mathf.RoundToInt(worldPos.y * 100 + height / 2);
+        return new Vector2Int(x, y);
+    }
+    private void UpdateTexture()
+    {
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                texture.SetPixel(x, y, grid[x, y].color); //pixel theo màu của Cell
+
+        texture.Apply();
+    }
+
+    private void SimulateSand()
+    {
+        for (int y = 1; y < height; y++) // khi grid[x, y-1] = grid[x, -1] y sẽ ngoài mảng, height -1 hàng trên cùng
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[x, y].type != EMaterialType.Sand)
+                    continue;
+                TryMoveSand(x, y);
+            }
+        }
+    }
+    private void TryMoveSand(int x, int y) // sand drop
+    {
+        // cạnh trái và phải màn hình
+        if(x==0|| x == width - 1)
+        {
+            HandleBorder(x, y);
+            return;
+        }
+        //check ô dưới
+        if (grid[x,y-1].type == EMaterialType.Empty)
+        {
+            Swap(x, y, x, y - 1);  // đổi y với vị trí y -1
+        }
+        //check ô bên dưới bên phải
+        else if (grid[x+1,y-1].type == EMaterialType.Empty)
+        {
+            Swap(x, y, x + 1, y - 1);
+        }
+        //roi xuong ô bên dưới bên trái
+        else if (grid[x - 1, y - 1].type == EMaterialType.Empty)
+        {
+            Swap(x, y, x - 1, y - 1);
+        }
+
+    }
+
+    private void HandleBorder(int x, int y)
+    {
+        //check ô dưới
+        if (grid[x, y - 1].type == EMaterialType.Empty)
+        {
+            Swap(x, y, x, y - 1);  // đổi y với vị trí y -1
+        }
+        else
+        {
+            if (x == 0)
+            {
+                //check ô bên dưới bên phải
+                if (grid[x + 1, y - 1].type == EMaterialType.Empty)
+                {
+                    Swap(x, y, x + 1, y - 1);
+                }
+            }
+            else if (x == width - 1)
+            {
+                //roi xuong ô bên dưới bên trái
+                if (grid[x - 1, y - 1].type == EMaterialType.Empty)
+                {
+                    Swap(x, y, x - 1, y - 1);
+                }
+
+            }
+        }
+    
+    }
+    private void Swap(int x1,int y1, int x2, int y2)// doi vi tri ô
+    {
+        Cell temp = grid[x1, y1];
+        grid[x1, y1] = grid[x2, y2];
+        grid[x2, y2] = temp;
     }
 }
